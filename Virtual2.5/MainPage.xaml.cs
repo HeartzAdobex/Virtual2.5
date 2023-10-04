cd8c375083250722d1add572529a4c3a0805c100
HeartzAdobex <nithikorn.boon@bu.ac.th>
2023-10-04 17:17:57 +0700

    AbsoluteLayout Event Error unfixed

--- Virtual2.5/MainPage.xaml.cs
+++ Virtual2.5/MainPage.xaml.cs
@@ -1,12 +1,31 @@
-﻿namespace Virtual2._5
+﻿using System.Drawing;
+
+namespace Virtual2._5
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        Microsoft.Maui.Graphics.Point center;
+        double radius;
 
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        void absoluteLayout_SizeChanged(object sender, EventArgs e)
+        {
+            /*center = new Microsoft.Maui.Graphics.Point(absoluteLayout.Width / 2, absoluteLayout.Height / 2);
+            radius = Math.Min(absoluteLayout.Width, absoluteLayout.Height) / 2;
+            AbsoluteLayout.SetLayoutBounds(image, new Rectangle(center.X - image.Width / 2),center.Y - radius,
+            AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));*/
+        }
+
+        async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            image.Rotation= 0;
+            image.AnchorY = radius / image.Height;
+            await image.RotateTo(360, 2000);
         }
     }
 }
