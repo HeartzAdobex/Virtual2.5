6324c6f0c27fc98d01e3d94aa6ad4ffa0ca75f32
Linlaboy <aut.beam@bumail.net>
2023-10-04 17:49:29 +0700

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalMall
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CalMall
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EquationPage(1));
+        }
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EquationPage(2));
+        }
+
+        async private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EquationPage(3));
+        }
+    }
+}
