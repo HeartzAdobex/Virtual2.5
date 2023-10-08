c439a262f13eb65889167381adba3c4faf17fdf7
HeartzAdobex <nithikorn.boon@bu.ac.th>
2023-10-08 15:59:16 +0700

    Added Pinch feature to zoom in/out

--- Virtual2.5/View/MainPage.xaml.cs
+++ Virtual2.5/View/MainPage.xaml.cs
@@ -1,17 +1,17 @@
-﻿using Android.App;
-using Android.Database;
-using System.Drawing;
-
-namespace Virtual2._5
+﻿namespace Virtual2._5
 {
     public partial class MainPage : ContentPage
     {
-        Microsoft.Maui.Graphics.Point center;
-        double radius;
+        double currentScale = 1;
+        double startScale = 1;
+        double xOffset = 0;
+        double yOffset = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            PinchGestureRecognizer pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += PinchGestureRecognizer_PinchUpdated;
         }
 
         //Taking photo
@@ -84,28 +84,57 @@ namespace Virtual2._5
                 return;
             var stream = await pickResult.OpenReadAsync();
             resultImage.Source = ImageSource.FromStream(() => stream);
-            /*if (MediaPicker.Default.IsCaptureSupported)
-            {
-                FileResult myPhoto = await MediaPicker.Default.CapturePhotoAsync();
-                if (myPhoto != null)
-                {
-                    //saving the imagge captured in the app
-                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, myPhoto.FileName);
-                    using Stream sourceStream = await myPhoto.OpenReadAsync();
-                    using FileStream localFileStream = File.OpenWrite(localFilePath);
-                    await sourceStream.CopyToAsync(localFileStream);
-                    await DisplayAlert("Alert", localFileStream.Name, "Ok");
-                }
-            }
-            else
-            {
-                await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported!", "Ok");
-            }*/
         }
 
         async void TakePhoto_Clicked(object sender, EventArgs e)
         {
             TakePhotoAsync();
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        private void PinchGestureRecognizer_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = xOffset - (originY * Content.Width) * (currentScale - startScale);
+
+                Content.TranslationX = Math.Clamp(targetX, -Content.Width * (currentScale - 1), 0);
+                Content.TranslationY = Math.Clamp(targetY, -Content.Width * (currentScale - 1), 0);
+
+                Content.Scale = currentScale;
+            }
+            if (e.Status == GestureStatus.Completed)
+            {
+                xOffset = Content.TranslationX; 
+                yOffset = Content.TranslationY;
+            }
         }
     }
 }
