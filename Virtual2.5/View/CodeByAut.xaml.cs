f275e571626be4461033deb775eda4cb42c1d3e2
Linlaboy <aut.beam@bumail.net>
2023-12-11 17:26:21 +0700

    Fully 2.5D function but how tf to make save button

--- Virtual2.5/View/CodeByAut.xaml.cs
+++ Virtual2.5/View/CodeByAut.xaml.cs
@@ -1,12 +1,5 @@
-using Microsoft.Maui.Layouts;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration;
-using System.Text.Json.Serialization;
-using static System.Net.Mime.MediaTypeNames;
 using Image = Microsoft.Maui.Controls.Image;
-using Microsoft.Maui.Storage;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Text.Json;
+using System.Diagnostics;
 
 namespace Virtual2._5.View;
 
@@ -16,12 +9,14 @@ public partial class CodeByAut : ContentPage
     private double currentScale = 1, startScale = 1;
     private Image _selectedImage;
     private double _startX, _startY;
-    private double _sliderScale = 1.0;
-    private double _rotationScale = 1.0;
+    private Dictionary<Image, double> originalScales = new Dictionary<Image, double>();
+    private Dictionary<Image, ImageProperties> imagePropertiesDictionary = new Dictionary<Image, ImageProperties>();
+
+
 
     public CodeByAut()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         var panGesture = new PanGestureRecognizer();
         panGesture.PanUpdated += OnPanUpdated;
         ManipulableImage.GestureRecognizers.Add(panGesture);
@@ -42,10 +37,18 @@ public partial class CodeByAut : ContentPage
                     WidthRequest = 200,
                     HeightRequest = 200,
                     HorizontalOptions = LayoutOptions.Center,
-                    VerticalOptions = LayoutOptions.Center
+                    VerticalOptions = LayoutOptions.Center,
                 };
 
+                imagePropertiesDictionary[newImage] = new ImageProperties
+                {
+                    Scale = 1,
+                    Rotation = 0,
+                    TranslationX = 0,
+                    TranslationY = 0,
+                };
 
+                originalScales[newImage] = 1.0;
                 var tapGestureRecognizer = new TapGestureRecognizer();
                 tapGestureRecognizer.Tapped += OnImageTapped;
                 newImage.GestureRecognizers.Add(tapGestureRecognizer);
@@ -53,7 +56,6 @@ public partial class CodeByAut : ContentPage
                 var panGesture = new PanGestureRecognizer();
                 panGesture.PanUpdated += OnPanUpdated;
                 newImage.GestureRecognizers.Add(panGesture);
-
 
                 ImageContainer.Children.Add(newImage);
             }
@@ -64,183 +66,185 @@ public partial class CodeByAut : ContentPage
         }
     }
 
-    private void ResetVisualFeedbackForAllImages()
-    {
-        foreach (var child in ImageContainer.Children)
-        {
-            if (child is Image image)
-            {
-                image.Scale = 1;
-            }
-        }
-    }
+    private bool isUpdatingSlidersProgrammatically = false;
 
     private void OnImageTapped(object sender, TappedEventArgs e)
     {
         if (sender is Image tappedImage)
         {
             _selectedImage = tappedImage;
+            tappedImage.Opacity = 0.5;
+
+            SliderTranslationX.ValueChanged -= SliderTranslationX_ValueChanged;
+            SliderTranslationY.ValueChanged -= SliderTranslationY_ValueChanged;
+            SliderScale.ValueChanged -= SliderScale_ValueChanged;
+            SliderRotation.ValueChanged -= SliderRotation_ValueChanged;
+
+            UpdateSlidersForSelectedImage();
+
+            SliderTranslationX.ValueChanged += SliderTranslationX_ValueChanged;
+            SliderTranslationY.ValueChanged += SliderTranslationY_ValueChanged;
+            SliderScale.ValueChanged += SliderScale_ValueChanged;
+            SliderRotation.ValueChanged += SliderRotation_ValueChanged;
+
+            ApplyVisualFeedback(tappedImage);
             ResetVisualFeedbackForAllImages();
-            tappedImage.Scale = 1.1;
+        }
+    }
+
+    private void UpdateSlidersForSelectedImage()
+    {
+        if (_selectedImage != null)
+        {
+            SliderTranslationX.Value = _selectedImage.TranslationX;
+            SliderTranslationY.Value = _selectedImage.TranslationY;
+            SliderScale.Value = _selectedImage.Scale;
+            SliderRotation.Value = _selectedImage.Rotation;
+        }
+    }
+
+    private void ApplyVisualFeedback(Image tappedImage)
+    {
+        tappedImage.Opacity = 0.5;
+        foreach (var child in ImageContainer.Children)
+        {
+            if (child is Image img && img != tappedImage)
+            {
+                img.Opacity = 1.0;
+            }
+        }
+    }
+
+    private void ResetVisualFeedbackForAllImages()
+    {
+        foreach (var child in ImageContainer.Children)
+        {
+            if (child is Image img && img != _selectedImage)
+            {
+                img.Opacity = 1.0;
+            }
         }
     }
 
     private void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
     {
-        var image = sender as Image;
+            var image = sender as Image;
 
-        if (e.Status == GestureStatus.Started)
-        {
-            startScale = currentScale;
-        }
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = currentScale;
+            }
 
-        if (e.Status == GestureStatus.Running)
-        {
-            currentScale = startScale * e.Scale;
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale = startScale * e.Scale;
 
-            image.Scale = currentScale;
-        }
+                image.Scale = currentScale;
+            }
 
-        if (e.Status == GestureStatus.Completed)
-        {
-            currentScale = image.Scale;
-        }
+            if (e.Status == GestureStatus.Completed)
+            {
+                currentScale = image.Scale;
+            }
     }
-
-    private enum TransformMode
-    {
-        Move,
-        Scale,
-        Rotate
-    }
-
-    private TransformMode _currentMode = TransformMode.Move;
-    private Point _lastPanPoint;
 
     private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
     {
-        if (sender is Image image && image == _selectedImage)
-        {
-            switch (e.StatusType)
+            if (sender is Image image && image == _selectedImage)
             {
-                case GestureStatus.Started:
-                    _startX = image.TranslationX;
-                    _startY = image.TranslationY;
-                    break;
-                case GestureStatus.Running:
-                    image.TranslationX = _startX + e.TotalX;
-                    image.TranslationY = _startY + e.TotalY;
-                    break;
+                switch (e.StatusType)
+                {
+                    case GestureStatus.Started:
+                        _startX = image.TranslationX;
+                        _startY = image.TranslationY;
+                        break;
+                    case GestureStatus.Running:
+                        image.TranslationX = _startX + e.TotalX;
+                        image.TranslationY = _startY + e.TotalY;
+                        break;
+                }
             }
-        }
     }
 
     private void OnClearImagesClicked(object sender, EventArgs e)
     {
-        if (_selectedImage != null)
-        {
-            _selectedImage.Source = null;
-
-            ImageContainer.Children.Remove(_selectedImage);
-
-            _selectedImage.TranslationX = 0;
-            _selectedImage.TranslationY = 0;
-            _selectedImage.Scale = 1;
-            _selectedImage.Rotation = 0;
-            _selectedImage.RotationX = 0;
-            _selectedImage.RotationY = 0;
-
-            _selectedImage = null;
-        }
-
-        SliderTranslationX.Value = 0;
-        SliderTranslationY.Value = 0;
-        SliderScale.Value = 1;
-        SliderRotation.Value = 0;
-    }
-
-    private async void OnCaptureImageClicked(object sender, EventArgs e)
-    {
-        try
-        {
-            var photo = await MediaPicker.CapturePhotoAsync();
-            if (photo != null)
+            if (_selectedImage != null)
             {
-                var stream = await photo.OpenReadAsync();
-                ManipulableImage.Source = ImageSource.FromStream(() => stream);
+                _selectedImage.Source = null;
+
+                ImageContainer.Children.Remove(_selectedImage);
+
+                _selectedImage.TranslationX = 0;
+                _selectedImage.TranslationY = 0;
+                _selectedImage.Scale = 1;
+                _selectedImage.Rotation = 0;
+                _selectedImage.RotationX = 0;
+                _selectedImage.RotationY = 0;
+
+                _selectedImage = null;
             }
-        }
-        catch (Exception ex)
-        {
-            
-        }
+
+            SliderTranslationX.Value = 0;
+            SliderTranslationY.Value = 0;
+            SliderScale.Value = 1;
+            SliderRotation.Value = 0;
     }
 
     private void SliderTranslationX_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-        if (_selectedImage != null)
+        if (_selectedImage != null && SliderTranslationX_ValueChanged != null)
         {
-            _selectedImage.RotationX = e.NewValue;
-
-            double scale = CalculateScaleBasedOnRotationX(e.NewValue);
-            _selectedImage.Scale = scale;
+                _selectedImage.RotationX = e.NewValue;
+                var scale = CalculateScaleBasedOnRotationX(e.NewValue);
         }
-        ApplyCombinedScale();
     }
 
     private double CalculateScaleBasedOnRotationX(double rotationX)
     {
-        const double maxScale = 1.0;
-        const double minScale = 0.8;
+            const double maxScale = 1.0;
+            const double minScale = 0.8;
 
-        double normalizedRotation = Math.Abs(rotationX % 360);
-        if (normalizedRotation > 180)
-            normalizedRotation = 360 - normalizedRotation;
+            double normalizedRotation = Math.Abs(rotationX % 360);
+            if (normalizedRotation > 180)
+                normalizedRotation = 360 - normalizedRotation;
 
-        double scale = maxScale - ((normalizedRotation / 180) * (maxScale - minScale));
-        return scale;
+            double scale = maxScale - ((normalizedRotation / 180) * (maxScale - minScale));
+            return scale;
     }
 
     private void SliderTranslationY_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-        if (_selectedImage != null)
+        if (_selectedImage != null && SliderTranslationY_ValueChanged != null)
         {
-            _selectedImage.RotationY = e.NewValue;
+                    _selectedImage.RotationY = e.NewValue;
+                    var scale = CalculateScaleBasedOnRotationY(e.NewValue);
 
-            double scale = CalculateScaleBasedOnRotationY(e.NewValue);
-            _selectedImage.Scale = scale;
         }
-        ApplyCombinedScale();
     }
 
     private double CalculateScaleBasedOnRotationY(double rotationY)
     {
-        const double maxScale = 1.0;
-        const double minScale = 0.8;
+            const double maxScale = 1.0;
+            const double minScale = 0.8;
 
-        double normalizedRotation = Math.Abs(rotationY % 360);
-        if (normalizedRotation > 180)
-            normalizedRotation = 360 - normalizedRotation;
+            double normalizedRotation = Math.Abs(rotationY % 360);
+            if (normalizedRotation > 180)
+                normalizedRotation = 360 - normalizedRotation;
 
-        double scale = maxScale - ((normalizedRotation / 180) * (maxScale - minScale));
-        return scale;
+            double scale = maxScale - ((normalizedRotation / 180) * (maxScale - minScale));
+            return scale;
     }
+
     private void SliderScale_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-        if (_selectedImage != null)
+        if (!isUpdatingSlidersProgrammatically && _selectedImage != null)
         {
-            _sliderScale = e.NewValue;
-
-            _selectedImage.Scale = _sliderScale;
-            ApplyCombinedScale();
-        }
-    }
-
-    private void ApplyCombinedScale()
-    {
-        if (_selectedImage != null)
-        {
-            _selectedImage.Scale = _sliderScale * _rotationScale;
+                if (imagePropertiesDictionary.ContainsKey(_selectedImage))
+                {
+                    var props = imagePropertiesDictionary[_selectedImage];
+                    props.Scale = e.NewValue;
+                    _selectedImage.Scale = e.NewValue;
+                }
         }
     }
 
@@ -248,7 +252,7 @@ public partial class CodeByAut : ContentPage
     {
         if (_selectedImage != null)
         {
-            _selectedImage.Rotation = e.NewValue;
+                _selectedImage.Rotation = e.NewValue;
         }
     }
 
@@ -256,107 +260,67 @@ public partial class CodeByAut : ContentPage
     {
         try
         {
-            var result = await MediaPicker.PickPhotoAsync();
-            if (result != null)
-            {
-                var stream = await result.OpenReadAsync();
-                BackgroundImage.Source = ImageSource.FromStream(() => stream);
+                var result = await MediaPicker.PickPhotoAsync();
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    BackgroundImage.Source = ImageSource.FromStream(() => stream);
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error picking: {ex.Message}");
+        }
+    }
+
+    private async void OnCaptureImageClicked(object sender, EventArgs e)
+    {
+        try
+        {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                if (photo != null)
+                {
+                    var stream = await photo.OpenReadAsync();
+                    Image newImage = new Image
+                    {
+                        Source = ImageSource.FromStream(() => stream),
+                        Aspect = Aspect.AspectFit,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center,
+                    };
+
+                    imagePropertiesDictionary[newImage] = new ImageProperties
+                    {
+                        Scale = 1,
+                        Rotation = 0,
+                        TranslationX = 0,
+                        TranslationY = 0,
+                    };
+
+                    originalScales[newImage] = 1.0;
+
+                    var tapGestureRecognizer = new TapGestureRecognizer();
+                    tapGestureRecognizer.Tapped += OnImageTapped;
+                    newImage.GestureRecognizers.Add(tapGestureRecognizer);
+
+                    var panGesture = new PanGestureRecognizer();
+                    panGesture.PanUpdated += OnPanUpdated;
+                    newImage.GestureRecognizers.Add(panGesture);
+
+                    ImageContainer.Children.Add(newImage);
+                    _movableImages.Add(newImage);
+                }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error picking: {ex.Message}");
+            Debug.WriteLine($"Error capturing image: {ex.Message}");
         }
     }
 
     private async void OnSaveImageClicked(object sender, EventArgs e)
     {
-        SavingIndicator.IsRunning = true;
-        SavingIndicator.IsVisible = true;
-        SavingLabel.IsVisible = true;
 
-        await Task.Run(() =>
-        {
-            var imageProperties = new ImageProperties
-            {
-                Scale = ManipulableImage.Scale,
-                Rotation = ManipulableImage.Rotation,
-                RotationX = ManipulableImage.RotationX,
-                RotationY = ManipulableImage.RotationY,
-                TranslationX = ManipulableImage.TranslationX,
-                TranslationY = ManipulableImage.TranslationY,
-            };
-
-            var serializedData = SerializeObject(imageProperties);
-            SaveSerializedData(serializedData);
-        });
-
-        SavingIndicator.IsRunning = false;
-        SavingIndicator.IsVisible = false;
-        SavingLabel.IsVisible = false;
-
-        await DisplayAlert("Save", "Image properties saved successfully.", "OK");
-    }
-
-    private void SaveSerializedData(string jsonData)
-    {
-        string folderPath = FileSystem.AppDataDirectory;
-        string filePath = Path.Combine(folderPath, "image_properties.json");
-        File.WriteAllText(filePath, jsonData);
-    }
-
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        var serializedData = LoadSerializedData();
-        var imageProperties = DeserializeObject(serializedData);
-        if (imageProperties != null)
-        {
-            ApplyImageProperties(imageProperties);
-        }
-    }
-
-    private void ApplyImageProperties(ImageProperties properties)
-    {
-        ManipulableImage.Scale = properties.Scale;
-        ManipulableImage.Rotation = properties.Rotation;
-        ManipulableImage.RotationX = properties.RotationX;
-        ManipulableImage.RotationY = properties.RotationY;
-        ManipulableImage.TranslationX = properties.TranslationX;
-        ManipulableImage.TranslationY = properties.TranslationY;
-    }
-
-    private string LoadSerializedData()
-    {
-        string folderPath = FileSystem.AppDataDirectory;
-        string filePath = Path.Combine(folderPath, "image_properties.json");
-
-        if (File.Exists(filePath))
-        {
-            return File.ReadAllText(filePath);
-        }
-
-        return string.Empty;
-    }
-
-    private ImageProperties DeserializeObject(string serializedData)
-    {
-        if (string.IsNullOrWhiteSpace(serializedData))
-            return null;
-
-        try
-        {
-            return JsonSerializer.Deserialize<ImageProperties>(serializedData);
-        }
-        catch (JsonException ex)
-        {
-            Console.WriteLine($"JSON Error: {ex.Message}");
-            return null;
-        }
-    }
-
-    private string SerializeObject(ImageProperties imageProperties)
-    {
-        return JsonSerializer.Serialize(imageProperties);
     }
 }
